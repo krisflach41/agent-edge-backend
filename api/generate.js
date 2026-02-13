43ff5de7b84c74f34d400545c5b7ca0044e3b2f0
kristyflach <krisflach12@gmail.com>
2026-02-13 08:00:41 -0500

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,7 +1,13 @@
 export default async function handler(req, res) {
-  // Enable CORS
+  // Enable CORS for both domains
+  const allowedOrigins = ['https://kristyflach.com', 'https://kristyflach41.github.io'];
+  const origin = req.headers.origin;
+  
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  
   res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
   
