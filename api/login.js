c853ddceb7ef9332cb2ccd07e2eca544b103c8ee
kristyflach <krisflach12@gmail.com>
2026-02-14 09:22:34 -0500

    Update login.js

--- api/login.js
+++ api/login.js
@@ -80,7 +80,8 @@ export default async function handler(req, res) {
         name: sheetResult.user.name,
         email: sheetResult.user.email,
         brokerage: sheetResult.user.brokerage
-      }
+      },
+      tempPassword: sheetResult.tempPassword || false
     });
 
   } catch (error) {
